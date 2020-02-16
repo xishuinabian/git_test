fdf23dd35aaa6955c4b7c5c67e099903dcd88f7b
jiangshan <jian.shan1989@163.com>
2020-02-16 20:37:58 +0800

    commit

---
+++ helloword.cpp
@@ -0,0 +1,12 @@
+// helloword.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "windows.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	MessageBox(NULL, L"Hello World", L"www.reversecore.com", MB_OK);
+	return 0;
+}
+
